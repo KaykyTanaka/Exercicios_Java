2901964d7a5e081a7ca18dc67a66bc711b496cab
unknown <tanaka.kayky@gmail.com>
2022-09-13 11:43:21 -0300

    exercícios Java

--- Lista-01/Argumentos.java
+++ Lista-01/Argumentos.java
@@ -2,8 +2,8 @@
  * 2 - Crie um programa utilizando a linguagem de programação Java. Esse programa deve exibir os
  * dois primeiros argumentos de linha de comando. O código fonte desse programa deve ser
  * armazenado em um arquivo chamado Argumentos.java
+ * 
  */
-
 public class Argumentos {
     public static void main(String[] args) {
         
