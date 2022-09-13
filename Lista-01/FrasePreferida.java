2ae31860de7446d0e0d4298ae05070e9fdac15d7
unknown <tanaka.kayky@gmail.com>
2022-09-13 12:10:05 -0300

    exercícios Java

---
+++ Lista-01/FrasePreferida.java
@@ -0,0 +1,5 @@
+public class FrasePreferida {
+    public static void main(String[] args) {
+        System.out.println("\"o real não está na saída nem na chegada: ele se dispõe para a gente é no meio da travessia\" -Guimarães Rosa");
+    }
+}
