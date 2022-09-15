56967046e1a5c125e8f9ea0a0092f0e92313c581
unknown <tanaka.kayky@gmail.com>
2022-09-14 23:18:48 -0300

    exercícios Java

--- Lista-02/ExibeNumeroAleatorio.java
+++ Lista-02/ExibeNumeroAleatorio.java
@@ -9,8 +9,11 @@ import java.util.Random;
 
 public class ExibeNumeroAleatorio {
     public static void main(String[] args) {
+
         Random aleatorio = new Random();
+
         double numeroAleatorio = aleatorio.nextInt(100);
         System.out.println(numeroAleatorio);
+
     }
 }
