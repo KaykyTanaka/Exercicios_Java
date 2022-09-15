cf2ee572f8d6159bf5ab55efe7cb81036d8c6b1f
unknown <tanaka.kayky@gmail.com>
2022-09-14 23:18:14 -0300

    exercícios Java

---
+++ Lista-02/ExibeNumeroAleatorio.java
@@ -0,0 +1,16 @@
+
+/* 
+ * 2 - Crie um arquivo chamado ExibeNumeroAleatorio.java. Implemente um programa em Java que
+ * gere um número real aleatório entre 0 e 100. Esse número deve ser armazenado em uma variável do
+ * tipo double chamada numeroAleatorio. Por fim, exiba o valor dessa variável na saída padrão.
+ * 
+ */
+import java.util.Random;
+
+public class ExibeNumeroAleatorio {
+    public static void main(String[] args) {
+        Random aleatorio = new Random();
+        double numeroAleatorio = aleatorio.nextInt(100);
+        System.out.println(numeroAleatorio);
+    }
+}
