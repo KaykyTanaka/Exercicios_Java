8e174392e7339780e79cab30202e264295db9b11
unknown <tanaka.kayky@gmail.com>
2022-12-05 00:12:30 -0300

    exercícios Java

--- Lista-06/Exercicio48.java
+++ Lista-06/Exercicio48.java
@@ -7,10 +7,28 @@
  */
 
 import java.util.Scanner;
+
 public class Exercicio48 {
-	
-	public static void main (String[] args) {
+
+	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
+		int qtd, cont, cont1 = 0;
+
+		System.out.print("Insira a quantidade de termos da sequencia de numeros primos: ");
+		qtd = leia.nextInt();
+
+		for (int i = 1; cont1 != qtd; i++) {
+			cont = 0;
+			for (int j = 1; j <= i; j++) {
+				if (i % j == 0) {
+					cont++;
+				}
+			}
+			if (cont == 2) {
+				System.out.print(i + " | ");
+				cont1++;
+			}
+		}
 
 	}
 }
